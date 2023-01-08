78af0d04b4d0c50bc4f4fd035403a1ac4d907395
MaciejZawadzki-Adamiak <zawmaciek@gmail.com>
2023-01-09 00:15:23 +0100

    zad 2 ok

--- OK/zad_2/main.py
+++ OK/zad_2/main.py
@@ -2,10 +2,7 @@ from mknapsack import solve_unbounded_knapsack
 
 value = [100, 200, 60, 150, 250]
 weights = [3, 5, 2, 4, 6]
-
 capacity = 50
-
-# Assign items repeatedly into the knapsack while maximizing profits
 res = solve_unbounded_knapsack(value, weights, capacity)
 for weight, count in zip(weights, res):
     print(f"Przedmiot o wadze {weight} zapakowany {count} razy")
