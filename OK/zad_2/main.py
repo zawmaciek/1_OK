43a4b201135cc39918006ea9fdcfb1c33080e132
MaciejZawadzki-Adamiak <zawmaciek@gmail.com>
2023-01-09 00:14:02 +0100

    zad 2 ok

--- OK/zad_2/main.py
+++ OK/zad_2/main.py
@@ -7,7 +7,7 @@ capacity = 50
 
 # Assign items repeatedly into the knapsack while maximizing profits
 res = solve_unbounded_knapsack(value, weights, capacity)
-for weight, count in zip(weights,res):
+for weight, count in zip(weights, res):
     print(f"Przedmiot o wadze {weight} zapakowany {count} razy")
-print(f"Wartosc: {sum([count*weight for count,weight in zip(value,res)])}")
-print(f"Waga calkowita: {sum([count*weight for count,weight in zip(weights,res)])}")
+print(f"Wartosc: {sum([count * weight for count, weight in zip(value, res)])}")
+print(f"Waga calkowita: {sum([count * weight for count, weight in zip(weights, res)])}")
